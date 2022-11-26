3916ff77d97e34003dbb58b14587c312520d4076
Rodrigo Alan Garcia Perez <rodrigo.agp96@gmail.com>
2022-11-26 12:14:58 -0600

    Adding script for checking row ids

---
+++ modulo-04/e-02-rowids.sql
@@ -0,0 +1,38 @@
+spool 'output.txt' create;
+connect rodrigo05/rodrigo05
+whenever sqlerror exit rollback
+
+prompt Mostrando los primeros 10 registros y sus rowids 
+
+select row_id, id
+from(
+  select rowid as row_id, id
+  from t01_id
+  order by id 
+) where rownum <=10;
+
+prompt Mostrando segmentos generados 
+select substr(rowid, 1, 6) segmento,
+  count(*) as total_registros
+from t01_id
+group by substr(rowid, 1, 6);
+
+pause [Enter] para continuar 
+
+prompt Mostrando data files y registros asignados 
+select substr(rowid, 7, 3) data_file,
+  count(*) as total_registros
+from t01_id
+group by substr(rowid, 7, 3);
+
+pause [Enter] para continuar 
+
+
+prompt Mostrando bloque de datos y registros incluidos en el 
+select substr(rowid, 10, 6) bloque,
+  count(*) as total_registros
+from t01_id
+group by substr(rowid, 10, 6);
+
+spool off;
+exit

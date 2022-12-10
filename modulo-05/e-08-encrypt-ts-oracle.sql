ee5eba9e139bdab905648b2e40acbc3a2ac800ba
Rodrigo Alan Garcia Perez <rodrigo.agp96@gmail.com>
2022-12-09 20:44:17 -0600

    Finishing encryption script

--- modulo-05/e-08-encrypt-ts-oracle.sql
+++ modulo-05/e-08-encrypt-ts-oracle.sql
@@ -49,3 +49,37 @@ prompt forzando sincronizacion, conectando como sys
 connect sys/system2 as sysdba
 
 alter system checkpoint;
+Pause [Enter] para realizar la busqueda del texto en el TS cifrado 
+!strings /u01/app/oracle/oradata/RGPDIP02/m05_encrypted_ts_01.dbf | grep "mensaje"
+
+--reiniciar y volver a mostrar los datos 
+Pause Reiniciando instancia [Enter] para continuar
+shutdown immediate
+startup 
+
+Prompt Consultando los datos nuevamente
+connect rodrigo05/rodrigo05
+
+select * from mensaje_seguro;
+
+Pause [Enter] para continuar y corregir el problema 
+
+connect sys/system2 as sysdba
+
+alter system set encryption wallet open identified by "wallet_password";
+
+Prompt Mostrando datos nuevamente
+connect rodrigo05/rodrigo05
+
+select * from mensaje_seguro;
+
+Pause [Enter] para realizar la limpieza
+
+drop table mensaje_seguro;
+drop table mensaje_inseguro;
+
+connect sys/system2 as sysdba 
+drop tablespace m05_encrypted_ts including contents and datafiles;
+
+Prompt Listo! 
+exit 
